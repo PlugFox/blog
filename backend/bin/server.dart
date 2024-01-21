2bba8f40b13525e0f4df6c2040a6ff4d7940a6d6
Plague Fox <plugfox@gmail.com>
2024-01-21 12:30:49 +0400

    Periodic task

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -4,7 +4,9 @@ import 'dart:convert';
 import 'package:backend/src/common/config/config.dart';
 import 'package:backend/src/common/config/initialization.dart';
 import 'package:backend/src/common/database/database.dart';
+import 'package:backend/src/common/medium/medium.dart';
 import 'package:backend/src/common/server/worker.dart';
+import 'package:http/http.dart' as http;
 import 'package:l/l.dart';
 
 /// Starts the server.
@@ -43,14 +45,18 @@ void main([List<String>? arguments]) => Future<void>.sync(() async {
           }
 
           // Periodic tasks
-          if (config.interval > 0 && config.username.isNotEmpty)
+          if (config.interval > 0 && config.username.isNotEmpty) {
+            final medium = Medium(
+              client: http.Client(),
+              database: database,
+            );
             Timer.periodic(
               Duration(seconds: config.interval),
-              (_) {
-                // TODO(plugfox): Fetch articles from medium by username
-              },
+              (_) => medium.fetchArticlesRSS(config.username).then<void>(medium.upsertArticlesIntoDatabase).ignore(),
             );
+          }
 
+          // Log server info
           l
             ..i('Started ${workers.length} worker(s) at '
                 '${config.address.host}:${config.port} in '
